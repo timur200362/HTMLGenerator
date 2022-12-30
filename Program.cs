5ddd7aed669b8ef3b98711dda5585ea19aa645d8
timur200362 <94241976+timur200362@users.noreply.github.com>
2022-12-30 18:54:42 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using HTMLGenerator.models;
+using HTMLGeneratorLibrary;
+using System;
+
+namespace HTMLGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = "Hello World";
+            var user = new User(343, "loginnew", "passwordnew", 18, "8987858594", "boku no pico");
+
+            string HtmlPage = "<html>\r\n<head>\r\n<title>Шапка</title>\r\n</head>\r\n<body>\r\n{{Id}}\r\n<p>Логин и пароль пользователя: <p>\r\n{{Login}}\r\n{{Password}}\r\n</body>\r\n</html>";
+
+            var gs = new HtmlGeneratorService();
+            var res = gs.GetHTMLMain(HtmlPage, user);
+
+            var n = 0;
+        }
+    }
+}
---
+++ models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTMLGenerator.models
+{
+    public class User : EntityBase
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+        public string Mobile { get; set; }
+        public string FavoriteAnimeName { get; set; }
+
+        public User() { }
+
+        public User(int id, string login, string password, int age, string mobile, string favoriteAnime)
+        {
+            Id = id;
+            Login = login;
+            Password = password;
+            Age = age;
+            Mobile = mobile;
+            FavoriteAnimeName = favoriteAnime;
+        }
+    }
+}
